d736b95b0ec056a27b10296ef542593d0856500f
Emil Lerch <emil@lerch.org>
2018-03-23 22:59:28 +0000

    AWS S3 Filesystem support - initial commit

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -223,6 +223,7 @@ object Dependencies {
   )
 
   private val awsCloudDependencies = List(
+    "com.fasterxml.jackson.core" % "jackson-annotations" % jacksonV,
     "software.amazon.awssdk" % "aws-sdk-java" % awsSdkV
   )
 
